0c7747ec8182c6a13f5f91294bc50ab70893f5ea
knbrlo <kenbrlow@gmail.com>
2020-10-09 19:23:26 -0600

    added requirements to environment file

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/great_game_getter/scraper'
+require_relative '../lib/great_game_getter/game'
+require_relative '../lib/great_game_getter/cli'
