69b2283669c2af757dc1a89a0a8f783184ca0c86
knbrlo <kenbrlow@gmail.com>
2020-10-09 19:28:55 -0600

    created methods to get pages

--- lib/great_game_getter/scraper.rb
+++ lib/great_game_getter/scraper.rb
@@ -1,3 +1,19 @@
 class GreatGameGetter::Scraper
-    
+
+    def get_page_new_trending
+        Nokogiri::HTML(open("https://store.steampowered.com/games/#p=0&tab=NewReleases"))
+    end
+
+    def get_page_top_sellers
+        Nokogiri::HTML(open("https://store.steampowered.com/games/#p=0&tab=TopSellers"))
+    end
+
+    def get_page_being_played
+        Nokogiri::HTML(open("https://store.steampowered.com/games/#p=0&tab=ConcurrentUsers"))
+    end
+
+    def get_page_coming_soon
+        Nokogiri::HTML(open("https://store.steampowered.com/games/#p=0&tab=ComingSoon"))
+    end
+
 end
